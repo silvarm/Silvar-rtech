cf237e70f461dc9329d27c9e1d3c90953392b228
tudeng <murusilvar1@gmail.com>
2017-10-05 14:28:18 +0300

    Lesson 4

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tudeng/silvar-rtech/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/tudeng/silvar-rtech/devel/lib:$LD_LIBRARY_PATH"
+export PWD="/home/tudeng/silvar-rtech/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tudeng/silvar-rtech/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/tudeng/silvar-rtech/src:$ROS_PACKAGE_PATH"
---
+++ build/my_r2d2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tudeng/silvar-rtech/src/my_r2d2
+# Build directory: /home/tudeng/silvar-rtech/build/my_r2d2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
